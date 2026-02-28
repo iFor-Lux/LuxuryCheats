fd5f707825e641086166e55dd969758c4d071af5
Luxury . dev <jhojantomairo2@gmail.com>
2026-02-28 16:25:26 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.luxury.cheats"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
